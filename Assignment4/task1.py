5a039b5dff5b80ba600375c71b7a67e7751df0f4
olbert <iampirate@rambler.ru>
2020-05-17 20:56:11 +0200

    Almost done. E and G left

--- Assignment4/task1.py
+++ Assignment4/task1.py
@@ -4,9 +4,13 @@ import matplotlib
 import os
 import sklearn
 from sklearn.decomposition import PCA
-from sklearn import decomposition
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn import datasets
+from pandas.plotting import scatter_matrix
 from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+import pandas as pd
+
 
 class DIR(object):
     __slots__ = ()
@@ -14,71 +18,139 @@ class DIR(object):
     RES = os.path.join(CHECKOUT, 'res')
     OUT = os.path.join(CHECKOUT, 'out')
 
-def draw3D(x, y, z, col, show = True):
-    fig = plt.figure()
-    # ax = fig.add_subplot(111, projection=matplotlib)
-    ax = Axes3D(fig)
-    #x, y, z = transformed[0, :], transformed[1, :], transformed[2, :]
-    ax.scatter(x, y, z, c = col)
-    if show:
-        plt.show()
-
-
-def PCA(data, dims=2):
-    mean = np.mean(data, axis=0)
-
-    data = data - mean
-
-    covariance = np.cov(data.T)
-    values, vectors = np.linalg.eig(covariance)
-
-    ind = np.argpartition(values, -dims)[-dims:]  # Get id of dims max values
-
-    # Check vectors and values
-    for i in range(len(values)):
-        eigv = vectors[:, i].reshape(1, data.shape[1]).T
-        np.testing.assert_array_almost_equal(covariance.dot(eigv), values[i] * eigv,
-                                             decimal=6, err_msg='', verbose=True)
-    vectors = vectors.T  # But Why??
-    if dims == 2:
-        principals = np.array((vectors[ind[0]], vectors[ind[1]]))
-    else:
-        if dims == 3:
-            principals = np.array((-vectors[ind[0]], -vectors[ind[1]], vectors[ind[2]]))
-    transformed = principals.dot(data.T).real
-
-    # print(transformed)
-    if dims == 2:
-        plt.scatter(transformed[0, :], transformed[1, :], c="r")
-        plt.show()
-    else:
-        if dims == 3:
-            draw3D(transformed[0, :], transformed[1, :], transformed[2, :], 'b')
-    pass
-
-
 
 DIR = DIR()
 input_file = os.path.join(DIR.RES, 'breast-cancer-wisconsin.data')
 
-# read data as 2D array of data type 'object'
-data = np.loadtxt(input_file, dtype=np.str, comments='#', delimiter=',').T
+"""
+#  Attribute                     Domain
+   -- -----------------------------------------
+   1. Sample code number            id number
+   2. Clump Thickness               1 - 10
+   3. Uniformity of Cell Size       1 - 10
+   4. Uniformity of Cell Shape      1 - 10
+   5. Marginal Adhesion             1 - 10
+   6. Single Epithelial Cell Size   1 - 10
+   7. Bare Nuclei                   1 - 10
+   8. Bland Chromatin               1 - 10
+   9. Normal Nucleoli               1 - 10
+  10. Mitoses                       1 - 10
+  11. Class:                        (2 for benign, 4 for malignant)
+"""
 
 
-data = np.where(data == '?', 0, data).astype(int)[1:11]
+"""         Task A          """
+# read data as collection of string
+mix_data = np.loadtxt(input_file, dtype=np.str, comments='#', delimiter=',').T
 
-plt.plot(data.T[0:1])
+# change all missing instances to 0 (why??)
+# also remove first and last column (id, class)
+data = np.where(mix_data == '?', 0, mix_data).astype(int)
+
+
+
+
+
+"""         Task B          """
+# Aplly PCA
+pca = PCA()
+principalComponents_breast = pca.fit_transform(data[1:10].T)
+# Make some plots
+plt.scatter(np.arange(data[1:10].shape[0]) + 1, pca.explained_variance_)
+plt.xlabel('components')
+plt.ylabel('variance')
 plt.show()
-arr = data
+
+plt.scatter(np.arange(data[1:10].shape[0]) + 1, pca.explained_variance_ratio_)
+plt.ylim(0, 1)
+plt.xlabel('components')
+plt.ylabel('variance ratio')
+plt.show()
+# Last plot summarizes cumulative variance ratio
+total_vars = [sum(pca.explained_variance_ratio_[0: current]) for current in
+              range(1, len(pca.explained_variance_ratio_) + 1, 1)]
+plt.scatter(np.arange(data[1:10].shape[0]) + 1, total_vars)
+plt.axhline(0.9, label='90% threshold')
+plt.legend()
+plt.ylim(0, 1)
+plt.xlabel('components')
+plt.ylabel('total variance ratio')
+plt.show()
+
+""" To cross 90% threshold we need at least 5 first components"""
 
 
-PCA(data, 3)
 
 
-pca = PCA(n_components=2)
-pca.fit(X)
-PCA(n_components=2)
 
-print(pca.explained_variance_ratio_)
+"""         Task C          """
 
-print(pca.singular_values_)
+pca = PCA(n_components=5)
+
+principalComponents_breast = pca.fit_transform(data[1:10].T)
+# Adding class column to data
+target = data[10:None].T
+principalComponents_breast = np.hstack((principalComponents_breast, target))
+
+principal_breast_Df = pd.DataFrame(data=principalComponents_breast,
+                                   columns=['pc1', 'pc2', 'pc3', 'pc4', 'pc5', 'target'])
+principal_breast_Df.tail()
+# Here seaborn is used to make scatter plot matrix
+g = sns.pairplot(principal_breast_Df, hue="target",vars=['pc1', 'pc2', 'pc3', 'pc4', 'pc5'])
+
+# New legend added because original one is badly located
+handles = g._legend_data.values()
+labels = ['Benign', 'Malignant']
+g.fig.legend(handles=handles, labels=labels, loc='lower center', ncol=3)
+plt.show()
+
+
+
+
+
+
+
+"""         Task D          """
+pca = PCA()
+
+principalComponents_breast = pca.fit_transform(data[1:10].T)
+# number of components
+n_pcs= pca.components_.shape[0]
+
+# get the index of the most important feature on each component
+most_important = [np.abs(pca.components_[i]).argmax() for i in range(n_pcs)]
+
+# using just numbers of features
+initial_feature_names = ['2','3','4','5', '6', '7', '8', '9', '10']
+# get the names
+most_important_names = [initial_feature_names[most_important[i]] for i in range(n_pcs)]
+
+# LIST COMPREHENSION HERE AGAIN
+dic = {'PC{}'.format(i): most_important_names[i] for i in range(n_pcs)}
+
+df = pd.DataFrame(dic.items())
+print("Sorted features from most to least important \n")
+print (df)
+
+""" As Written in the output:
+the most important feature is 7 (Bare Nuclei) 
+the least important feature is 3 (Uniformity of Cell Size) """
+
+
+
+
+
+"""         Task E          """
+
+
+"""         Task F          """
+"""
+Since PCA is based on the correlation matrix, it is better to scale the data bere processing. 
+Without pre-processing huge variance in one variable can affect correlation matrix.
+To avoid it, we have to scale all the variables to zero mean and variance of 1
+"""
+"""         Task G          """
+
+# lda = LinearDiscriminantAnalysis()
+#
+# lda.fit()
